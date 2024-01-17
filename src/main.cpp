2aae4804052b35228ea37bf3f9cfbed0657a69e3
mdafrozalam <siddiquiafroz15@gmail.com>
2024-01-17 23:14:43 +0530

    Main: Added functions to create Heart ❤ and Circle ⭕ shape explosion.

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ class Particle {
         accn.y /= 1.05f;
 
         // Updating alpha color
-        alpha -= 0.025;
+        alpha -= 0.015;
         color = ColorAlpha(color, alpha);
       }
 
@@ -67,7 +67,7 @@ class Particle {
 
       // Decreasing volume of non-exploder
       if(!exploder){
-        SetSoundVolume(wistle, map(accn.y, 0, initAccnY, 0, 0.8f));
+        SetSoundVolume(wistle, map(accn.y, 0, initAccnY, 0, 0.5f));
       }
     }
 
@@ -90,13 +90,89 @@ class Particle {
 
 // Function to create exploder
 void explode(std::vector<Particle> &exploders, Vector2 pos, Color color, Sound wistle, Sound brust){
-  for(int i = 0; i < 100; i++){
+  for(int i = 0; i < 600; i++){
     exploders.push_back(
       Particle(
         pos,
-        //{0, GetRandomValue(100, 120)/1000.0f},
-        {0, 0.5},
+        {0, GetRandomValue(180, 220)/1000.0f},
         {GetRandomValue(-800, 800)/91.0f, (float)GetRandomValue(-400, 1200)/91.0f},
+        color,
+        wistle,
+        brust,
+        true
+      )
+    );
+  }
+}
+
+// Circle Explosion
+void explodeCircle(std::vector<Particle> &exploders, Vector2 pos, Color color, Sound wistle, Sound brust){
+  for(float a = 0; a < PI * 2; a+=0.01){
+    exploders.push_back(
+      Particle(
+        pos,
+        {0, 0.2},
+        {cos(a) * GetRandomValue(1, 10), sin(a) * GetRandomValue(1, 10)},
+        color,
+        wistle,
+        brust,
+        true
+      )
+    );
+  }
+}
+
+// Heart Explosion
+int sgn(double d){
+	return (d > 0) ? 1 : (d < 0 ? -1 : 1);
+}
+void explodeHeart(std::vector<Particle> &exploders, Vector2 pos, Color color, Sound wistle, Sound brust){
+  // Bottom Heart
+  for(float ang = 0; ang > -PI ; ang-=0.005){
+    // This formula is used to create SuperShape
+    float x = pow(abs(cos(ang)), 2) * GetRandomValue(400, 850)/100.0f  * sgn(cos(ang));
+		float y = pow(abs(sin(ang)), 2) * GetRandomValue(400, 850)/100.0f * sgn(sin(ang));
+    exploders.push_back(
+      Particle(
+        pos,
+        {0, GetRandomValue(180, 220)/1000.0f},
+        {x, y},
+        color,
+        wistle,
+        brust,
+        true
+      )
+    );
+  }
+  // Left Arc
+  for(float ang = -PI/4; ang < 3 * PI / 4; ang+=0.01){
+    // This formula is used to create SuperShape
+    float x = pow(abs(cos(ang)), 1) * GetRandomValue(250, 560)/100.0f * sgn(cos(ang));
+		float y = pow(abs(sin(ang)), 1) * GetRandomValue(250, 560)/100.0f * sgn(sin(ang));
+    exploders.push_back(
+      Particle(
+        //{pos.x - 60, pos.y - 60},
+        pos,
+        {0, GetRandomValue(180, 220)/1000.0f},
+        {x + 4, y + 4},
+        color,
+        wistle,
+        brust,
+        true
+      )
+    );
+  }
+  // Right Arc
+  for(float ang = PI / 4; ang < 5 * PI / 4; ang+=0.01){
+    // This formula is used to create SuperShape
+    float x = pow(abs(cos(ang)), 1) * GetRandomValue(250, 560)/100.0f * sgn(cos(ang));
+		float y = pow(abs(sin(ang)), 1) * GetRandomValue(250, 560)/100.0f * sgn(sin(ang));
+    exploders.push_back(
+      Particle(
+        //{pos.x - 60, pos.y - 60},
+        pos,
+        {0, GetRandomValue(180, 220)/1000.0f},
+        {x - 4, y + 4},
         color,
         wistle,
         brust,
@@ -132,8 +208,10 @@ int main() {
   // Main game loop
   while (!WindowShouldClose()) {
     // Adding crackers
-    //if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-    if(GetRandomValue(0, 100) < 2) {
+    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+      explodeHeart(exploders, {(float)GetMouseX(), (float)GetMouseY()}, WHITE, wistle_firework, brust_firework);
+    }
+    if(GetRandomValue(0, 100) < 3) {
       crackers.push_back(
         Particle(
           {(float)GetRandomValue(0, WINDOW_WIDTH), WINDOW_HEIGHT}, 
@@ -149,7 +227,7 @@ int main() {
 
     // Drawing on Texture
     BeginTextureMode(canvas);
-      DrawRectangle(0,0,WINDOW_WIDTH, WINDOW_HEIGHT, ColorAlpha(BLACK, 0.5));
+      DrawRectangle(0,0,WINDOW_WIDTH, WINDOW_HEIGHT, ColorAlpha(BLACK, 0.45));
       DrawText("Hello, 2G-Afroz!", 10, 10, 20, WHITE);
 
       // Crackers Loop
@@ -159,7 +237,13 @@ int main() {
         if(crackers.at(i).getAccn().y <= 0){
           // remeove that cracker;
           PlaySound(brust_firework);
-          explode(exploders, crackers.at(i).getPos(), crackers.at(i).getColor(), wistle_firework, brust_firework);
+          int random = GetRandomValue(0, 29);
+          if(random < 10)
+            explodeHeart(exploders, crackers.at(i).getPos(), crackers.at(i).getColor(), wistle_firework, brust_firework);
+          else  if(random < 20)
+                  explodeCircle(exploders, crackers.at(i).getPos(), crackers.at(i).getColor(), wistle_firework, brust_firework);
+              else
+                  explode(exploders, crackers.at(i).getPos(), crackers.at(i).getColor(), wistle_firework, brust_firework);
           crackers.erase(crackers.begin() + i);
         }
       }
